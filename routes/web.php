356271e392e99725c7fb53d28a8424418655ee5d
tuyenhoang0320i1 <hoangtuyen9x@gmail.com>
2020-07-31 13:55:48 +0700

    the same code in router house

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,6 @@ Route::get('register','LoginController@showFormRegister');
 Route::post('login','LoginController@login')->name('user.login');
 Route::post('register', 'LoginController@register')->name('user.register');
 
-
-Route::prefix('houses')->group(function () {
-    Route::get('/', 'HouseController@index')->name('houses.list');
-    Route::get('/show', 'HouseController@show')->name('houses.show');
-});
-
 Route::get('/home', function () {
     return view('homes/home');
 })->name('home');
