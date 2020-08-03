5268abe316f580d8f0e99dc9525c75dd7d9d8126
matkhau@123 <buimanhthangtb@gmail.com>
2020-08-03 11:42:41 +0700

    fix

--- resources/views/houses/list.blade.php
+++ resources/views/houses/list.blade.php
@@ -1,74 +1,27 @@
 @extends('layout.master')
 @section('content')
-    <div class="site-wrap">
-        <div class="site-mobile-menu site-navbar-target">
-            <div class="site-mobile-menu-header">
-                <div class="site-mobile-menu-close mt-3">
-                    <span class="icon-close2 js-menu-toggle"></span>
-                </div>
-            </div>
-            <div class="site-mobile-menu-body"></div>
-        </div>
-
-    </div>
-
-    <div class="site-mobile-menu site-navbar-target">
-        <div class="site-mobile-menu-header">
-            <div class="site-mobile-menu-close mt-3">
-                <span class="icon-close2 js-menu-toggle"></span>
-            </div>
-        </div>
-        <div class="site-mobile-menu-body"></div>
-    </div>
-
-    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-6 col-xl-2">
-                    <h1 class="mb-0 site-logo m-0 p-0"><a href="#" class="mb-0">Warehouse</a></h1>
-                </div>
-                <div class="col-12 col-md-10 d-none d-xl-block">
-                    <nav class="site-navigation position-relative text-right" role="navigation">
-                        <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-                            <li><a href="#home-section" class="nav-link">Home</a></li>
-                            <li><a href="{{route('houses.postForm')}}" >Đăng bài</a></li>
-                            <li><a href="#agents-section" class="nav-link">Agents</a></li>
-                            <li><a href="#about-section" class="nav-link">About</a></li>
-                            <li><a href="" class="nav-link">News</a></li>
-                            <li><a href="#contact-section" class="nav-link">Contact</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3"><a href="#"
-                                                                            class="site-menu-toggle js-menu-toggle text-white float-right"><span
-                            class="icon-menu h3"></span></a></div>
-            </div>
-        </div>
-    </header>
     <div class="site-block-wrap">
         <div class="owl-carousel with-dots">
-            <div class="site-blocks-cover overlay overlay-3"
-                 style="background-image: url({{asset('images/icons/82859229-sea-view-living-room-with-wooden-floor-and-empty-white-wall-background-in-luxury-beach-house-modern-.jpg')}});"
+            <div class="site-blocks-cover overlay overlay-2"
+                 style="background-image: url({{asset('i')}});"
                  data-aos="fade" id="home-section">
                 <div class="container">
                     <div class="row align-items-center justify-content-center">
                         <div class="col-md-6 mt-lg-5 text-center">
-                            <p class="mb-5 text-shadow text-capitalize text-black">Mái ấm thân thương. Đây là nơi để tìm
-                                hạnh phúc. Nếu không tìm
+                            <p class="mb-5 text-shadow text-capitalize text-black">Mái ấm thân thương. Đây là nơi để tìm hạnh phúc. Nếu không tìm
                                 thấy hạnh phúc ở đây, người ta không thể tìm thấy hạnh phúc ở bất cứ nơi nào khác.
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="site-blocks-cover overlay overlay-3"
-                 style="background-image: url({{asset('images/icons/90531026-indoor-plant-on-wooden-coffee-table-and-modern-furniture-with-empty-white-concrete-wall-background-b.jpg')}});"
+            <div class="site-blocks-cover overlay overlay-2"
+                 style="background-image: url({{asset('images/hero_2.jpg')}});"
                  data-aos="fade" id="home-section">
                 <div class="container">
                     <div class="row align-items-center justify-content-center">
                         <div class="col-md-6 mt-lg-5 text-center">
-                            <p class="mb-5 text-shadow text-capitalize text-black">Mỗi một con người, dù đi khắp tứ
-                                phương, rốt cục cũng chỉ muốn
+                            <p class="mb-5 text-shadow text-capitalize text-black">Mỗi một con người, dù đi khắp tứ phương, rốt cục cũng chỉ muốn
                                 tìm một mái nhà để dừng chân ghé về. Có được một nơi gọi là nhà, có được một người
                                 thương yêu để cùng chia sẻ vui buồn.
                             </p>
