1376f41a2036626244cc1557df0f5a1428896b4f
minhduc19669 <phongnhiryan@gmail.com>
2020-08-03 13:03:27 +0700

    out

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
 use App\Http\Requests\ValidateRegister;
-use App\User;
+
+
 
 
 class LoginController extends Controller
@@ -15,23 +21,42 @@ class LoginController extends Controller
     public function showFormRegister(){
         return view('users/register');
     }
-    public function login(){
 
+    public function login(Request $request){
+        $name=$request->username;
+        $password=md5($request->password);
+        $user = User::where([
+            ['name', '=', $name],
+            ['password', '=', $password],
+        ])->first();
+
+        if($user){
+            $login=$user->count();
+            if($login>0){
+                Session::put('user',$login->name);
+                return \view('houses.list');
+            }else{
+                Session::put('error','Sai tên đăng nhập hoặc mật khẩu!');
+                return \redirect()->route('user.login');
+            }
+        }
     }
+
     public function register(ValidateRegister $request){
         $name=$request->username;
         $email=$request->email;
         $password=$request->password;
         $phone=$request->phone;
+        $role=$request->role;
         $address=$request->address;
         User::create([
             'name'=>$name,
             'email'=>$email,
             'password'=>$password,
+            'role'=>$role,
             'address'=>$address,
             'phone'=>$phone
         ]);
-        return redirect()->route('home');
+        return redirect()->route('user.login');
     }
-
 }
--- resources/views/houses/post-form.blade.php
+++ resources/views/houses/post-form.blade.php
@@ -14,15 +14,10 @@
     <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
-
     <link rel="stylesheet" href="{{asset('css/jquery.fancybox.min.css')}}">
-
     <link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css')}}">
-
     <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
-
     <link rel="stylesheet" href="{{asset('css/aos.css')}}">
-
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
@@ -37,33 +32,41 @@
         </div>
         <div class="site-mobile-menu-body"></div>
     </div>
-{{--    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">--}}
-{{--        <div class="container">--}}
-{{--            <div class="row align-items-center">--}}
-{{--                <div class="col-6 col-xl-2">--}}
-{{--                    <h1 class="mb-0 site-logo m-0 p-0"><a href="#" class="mb-0">Warehouse</a></h1>--}}
-{{--                </div>--}}
-{{--                <div class="col-12 col-md-10 d-none d-xl-block">--}}
-{{--                    <nav class="site-navigation position-relative text-right" role="navigation">--}}
-{{--                        <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">--}}
-{{--                            <li><a href="#home-section" class="nav-link">Home</a></li>--}}
-{{--                            <li><a href="{{route('houses.postForm')}}">Đăng bài</a></li>--}}
-{{--                            <li><a href="#agents-section" class="nav-link">Agents</a></li>--}}
-{{--                            <li><a href="#about-section" class="nav-link">About</a></li>--}}
-{{--                            <li><a href="#news-section" class="nav-link">News</a></li>--}}
-{{--                            <li><a href="#contact-section" class="nav-link">Contact</a></li>--}}
-{{--                        </ul>--}}
-{{--                    </nav>--}}
-{{--                </div>--}}
-{{--                <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3"><a href="#"--}}
-{{--                                                                            class="site-menu-toggle js-menu-toggle text-white float-right"><span--}}
-{{--                            class="icon-menu h3"></span></a></div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </header>--}}
+
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-6 col-xl-2">
+
+                </div>
+                <div class="col-12 col-md-10 d-none d-xl-block">
+                    <nav class="site-navigation position-relative text-right" role="navigation">
+                        <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                            <li><a href="#home-section" class="nav-link">Home</a></li>
+                            <li><a href="{{route('houses.postForm')}}">Đăng bài</a></li>
+                            <li><a href="#agents-section" class="nav-link">Agents</a></li>
+                            <li><a href="#about-section" class="nav-link">About</a></li>
+                            <li><a href="#news-section" class="nav-link">News</a></li>
+                            <li><a href="#contact-section" class="nav-link">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3"><a href="#"
+                                                                            class="site-menu-toggle js-menu-toggle text-white float-right"><span
+                            class="icon-menu h3"></span></a></div>
+            </div>
+        </div>
+    </header>
+    <div class="site-blocks-cover inner-page-cover overlay"
+         style="background-image: url({{asset('images/hero_1.jpg')}});" data-aos="fade">
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+
+            </div>
+        </div>
+    </div>
 </div>
 <div class="container pt-5">
-
     <div class="card">
         <div class="card-header">
             <h4>Đăng bài cho thuê nhà</h4>
@@ -79,7 +82,6 @@
                     <div class="form-group">
                         <label>Type:</label>
                         <input type="text" name="type" class="form-control">
-
                     </div>
                     <div class="form-group">
                         <label>Chọn tỉnh/thành phố:</label>
@@ -101,31 +103,26 @@
                         </select>
                     </div>
                     <div class="form-group">
-                        <label>Số nhà :</label>
-                        <input type="text" name="sn" class="form-control">
-
+                        <label>Địa chỉ :</label>
+                        <textarea name="sn" class="form-control" rows="3"></textarea>
                     </div>
                     <div class="form-group">
                         <label>Rooms:</label>
                         <input type="text" name="rooms" class="form-control">
-
                     </div>
                     <div class="form-group">
                         <label>Price:</label>
                         <input type="text" name="price" class="form-control">
-
-                    </div>
+                        </div>
                     <div class="form-group">
                         <button type="submit" class="btn btn-primary">Đăng bài</button>
-
+                        <a href="{{route('houses.list')}}" class="btn btn-secondary">Thoát</a>
                     </div>
                 </form>
             </div>
-
         </div>
     </div>
 </div>
-
 <a href="#top" class="gototop"><span class="icon-angle-double-up"></span></a>
 <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
 <script src="{{asset('js/jquery-ui.js')}}"></script>
@@ -139,7 +136,6 @@
 <script src="{{asset('js/jquery.fancybox.min.js')}}"></script>
 <script src="{{asset('js/jquery.sticky.js')}}"></script>
 <script src="{{asset('js/main.js')}}"></script>
-
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
         crossorigin="anonymous"></script>
@@ -148,3 +144,4 @@
         crossorigin="anonymous"></script>
 </body>
 </html>
+
