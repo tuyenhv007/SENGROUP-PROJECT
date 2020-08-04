f2f758ce17fb023c44dd57632faf9fbd0eea22f5
matkhau@123 <buimanhthangtb@gmail.com>
2020-08-04 14:00:59 +0700

    fix

--- resources/views/houses/list.blade.php
+++ resources/views/houses/list.blade.php
@@ -52,32 +52,71 @@
             </div>
         </div>
     </div>
-    <div class="container pt-5">
+    <div class="container pt-3">
         <div class="card">
             <div class="card-body">
                 <form action="#" method="post" novalidate="novalidate">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="row">
-                                <div class="col-lg-3 col-md-3 col-sm-12 p-0 ml-5">
-                                    <input type="text" class="form-control search-slt" placeholder="Enter Pickup City">
+                    <div class="col ml-3">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6 col-sm-12 p-0 ml-4">
+                                        <input type="text" class="form-control search-slt"
+                                               placeholder="Enter Pickup City">
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-12 p-0 ml-4">
+                                        <button type="button" class="btn btn-danger wrn-btn">Search</button>
+                                    </div>
                                 </div>
-                                <div class="col-lg-3 col-md-3 col-sm-12 p-0 ml-2">
-                                    <input type="text" class="form-control search-slt" placeholder="Enter Drop City">
+                            </div>
+                        </div>
+                        <div class="row pt-1">
+                            <div class="col-lg-12">
+                                <div class="row">
+                                    <div class="col-lg-3 col-md-3 col-sm-12 p-0 ml-4">
+                                        <input type="text" class="form-control search-slt"
+                                               placeholder="Enter Pickup City">
+                                    </div>
+                                    <div class="col-lg-4 col-md-4 col-sm-12 p-0 ml-2">
+                                        <input type="text" class="form-control search-slt"
+                                               placeholder="Enter Drop City">
+                                    </div>
+                                    <div class="col-lg-4 col-md-4 col-sm-12 p-0 ml-2">
+                                        <select class="form-control search-slt" id="exampleFormControlSelect1">
+                                            <option>Select Vehicle</option>
+                                            <option>Example one</option>
+                                            <option>Example one</option>
+                                            <option>Example one</option>
+                                            <option>Example one</option>
+                                            <option>Example one</option>
+                                            <option>Example one</option>
+                                        </select>
+                                    </div>
                                 </div>
-                                <div class="col-lg-3 col-md-3 col-sm-12 p-0 ml-2">
-                                    <select class="form-control search-slt" id="exampleFormControlSelect1">
-                                        <option>Select Vehicle</option>
-                                        <option>Example one</option>
-                                        <option>Example one</option>
-                                        <option>Example one</option>
-                                        <option>Example one</option>
-                                        <option>Example one</option>
-                                        <option>Example one</option>
-                                    </select>
-                                </div>
-                                <div class="col-lg-2 col-md-2 col-sm-12 p-0 ml-4">
-                                    <button type="button" class="btn btn-danger wrn-btn">Search</button>
+                            </div>
+                        </div>
+                        <div class="row pt-1">
+                            <div class="col-lg-12">
+                                <div class="row">
+                                    <div class="col-lg-3 col-md-3 col-sm-12 p-0 ml-4">
+                                        <input type="text" class="form-control search-slt"
+                                               placeholder="Enter Pickup City">
+                                    </div>
+                                    <div class="col-lg-4 col-md-4 col-sm-12 p-0 ml-2">
+                                        <input type="text" class="form-control search-slt"
+                                               placeholder="Enter Drop City">
+                                    </div>
+                                    <div class="col-lg-4 col-md-4 col-sm-12 p-0 ml-2">
+                                        <select class="form-control search-slt" id="exampleFormControlSelect1">
+                                            <option>Select Vehicle</option>
+                                            <option>Example one</option>
+                                            <option>Example one</option>
+                                            <option>Example one</option>
+                                            <option>Example one</option>
+                                            <option>Example one</option>
+                                            <option>Example one</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -86,31 +125,41 @@
             </div>
         </div>
     </div>
-    <div class="site-section" id="properties-section">
-        <div class="container">
-            <div class="row large-gutters">
-                @foreach($houses as $key => $house)
-                    <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
-                        <div class="ftco-media-1">
-                            <div class="ftco-media-1-inner">
-                                <a href="{{route('houses.show',$house->id)}}" class="d-inline-block mb-4"><img
-                                        src="{{asset("storage/".$house->images[0]->image)}}"
-                                        alt="FImageo"
-                                        class="img-fluid" style="height: 500px;width: 300px;margin: auto"></a>
-                                <div class="ftco-media-details">
-                                    <h3>{{$house->name}}</h3>
-                                    <p>{{$house->addresses[0]->road}},
-                                        {{$house->addresses[0]->district}},
-                                        {{$house->addresses[0]->city}}</p>
+    <div class="container pt-2">
+        <div class="card">
+            <div class="card-body">
+                <div class="site-section" id="properties-section">
+                    <div class="container">
+                        <div class="row large-gutters">
+                            @foreach($houses as $key => $house)
+                                <div class="col-md-6 col-lg-4 mb-5 mb-lg-5 ">
+                                    <div class="ftco-media-1">
+                                        <div class="ftco-media-1-inner">
+                                            <a href="{{route('houses.show',$house->id)}}"
+                                               class="d-inline-block mb-4"><img
+                                                    src="{{asset("storage/".$house->images[0]->image)}}"
+                                                    alt="FImageo"
+                                                    class="img-fluid"
+                                                    style="height: 500px;width: 300px;margin: auto"></a>
+                                            <div class="ftco-media-details">
+                                                <h3>{{$house->name}}</h3>
+                                                <p>{{$house->addresses[0]->road}},
+                                                    {{$house->addresses[0]->district}},
+                                                    {{$house->addresses[0]->city}}</p>
 
-                                    <strong>{{$house->price}}</strong>
+                                                <strong>{{$house->price}}</strong>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                            @endforeach
                         </div>
                     </div>
-                @endforeach
+                </div>
+
             </div>
         </div>
     </div>
+
 @endsection
 
