cb18aa89c26e61ac386695f9341da4f580bb753d
matkhau@123 <buimanhthangtb@gmail.com>
2020-08-04 17:55:36 +0700

    fix

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,7 +8,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="{{asset('js/ajax-city.js')}}"></script>
+    <script src="{{asset('js/ajax-city1.js')}}"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
