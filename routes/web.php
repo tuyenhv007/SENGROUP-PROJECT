750cb0a821340d2381782e890df790d1b29753fc
tuyenhoang0320i1 <hoangtuyen9x@gmail.com>
2020-08-06 15:02:25 +0700

    completed feature forgot password and get password with email

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,11 @@ Route::post('/login', 'LoginController@login')->name('user.login');
 Route::post('/register', 'LoginController@register')->name('user.register');
 Route::get('/logout', 'LoginController@logout')->name('logout');
 
+Route::get('/forgot-password', 'ForgotPasswordController@index')->name('forgot.index');
+Route::post('/forgot-password', 'ForgotPasswordController@sendCodeResetPassword')->name('forgot.sendCode');
+
+Route::get('/reset-password', 'ForgotPasswordController@resetPassword')->name('reset.password');
+Route::post('/reset-password', 'ForgotPasswordController@saveResetPassword')->name('save.reset.password');
 //Route::get('/user/redirect','SocialController@redirectToProvider')->name('user.redirect');
 //Route::get('/user/callback','SocialController@handleProviderCallback');
 
@@ -31,6 +36,7 @@ Route::prefix('user')->group(function () {
     Route::post('/profile/update/avatar/{id}', 'UserController@updateAvatar')->name('user.edit.avatar');
     Route::get('/change-password/{id}','UserController@formChangePassword')->name('user.formChangePassword');
     Route::post('/change-password/{id}','UserController@changePassword')->name('user.changePassword');
+
 });
 
 
