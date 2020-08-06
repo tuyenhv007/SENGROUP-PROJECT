c343508765a45674df61281c540227440c9d5423
minhduc19669 <phongnhiryan@gmail.com>
2020-08-06 08:47:31 +0700

    oki

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -57,7 +57,7 @@
         <div class="row align-items-center">
             <div class="col-lg-12">
                 <nav class="navbar navbar-expand-lg navbar-light">
-                    <a style="font-size:190%;font-style: inherit; margin-top: 6px" class="navbar-brand"
+                    <a style="font-size:190%;font-style: oblique;font-family: Consolas; margin-top: 6px" class="navbar-brand"
                        href="{{route('houses.list')}}">
                         SENGROUP</a>
 
@@ -89,7 +89,7 @@
                                     <div style="margin-left: 30px " class="dropdown-menu"
                                          aria-labelledby="navbarDropdown">
 
-                                        <a  style="text-align: center" class="dropdown-item" href="">Tin đã đăng</a>                     
+                                        <a  style="text-align: center" class="dropdown-item" href="">Tin đã đăng</a>
                                         <a style="text-align: center" class="dropdown-item"
                                            href="{{route('user.show',['id'=>Session::get('user')->id])}}">Hồ sơ
                                         </a>
@@ -98,8 +98,6 @@
                                         </a>
                                         <a style="text-align: center" class="dropdown-item" href="{{route('logout')}}">Đăng
                                             xuất</a>
-
-
                                     </div>
                                 </li>
                             @else
